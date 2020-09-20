4cab1698e46ea7a073aaf4352d5e82123fefdce4
khasmamad99 <sh.khasmamad@gmail.com>
2020-09-21 00:40:19 +0300

    fixed checkpoint path

--- utils.py
+++ utils.py
@@ -169,7 +169,7 @@ def get_training_dataloader(mean, std, batch_size=16, num_workers=2, shuffle=Tru
     #cifar100_training = CIFAR100Train(path, transform=transform_train)
     cifar100_training = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform_train)
     cifar100_training_loader = DataLoader(
-        cifar100_training, shuffle=shuffle, num_workers=num_workers, batch_size=batch_size)
+        cifar100_training, shuffle=shuffle, num_workers=num_workers, batch_size=batch_size, drop_last=True)
 
     return cifar100_training_loader
 
